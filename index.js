aad13748a27e9d5de8b54b5da36d9ad63eeaed9a
avinash singh <avinash8.as@gmail.com>
2023-01-11 17:09:05 -0500

    add first event listener

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cocktailImgs = document.querySelector("#cocktail-menu")
-
+const cocktailNames = document.querySelector("#drink-name")
 
 fetch('http://localhost:3000/drinks')
 .then(resp => resp.json())
@@ -7,6 +7,9 @@ fetch('http://localhost:3000/drinks')
     drinks.forEach(drink => {
         const img = document.createElement('img')
         img.src = drink.strDrinkThumb
+        img.addEventListener('mouseover', e => {
+            cocktailNames.innerText = drink.strDrink
+        })
         cocktailImgs.append(img)
     })
 })
