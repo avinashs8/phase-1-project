316bea412cbe727b85f0dccfbf383207d742843d
avinash singh <avinash8.as@gmail.com>
2023-01-11 17:33:47 -0500

    render measures

--- index.js
+++ index.js
@@ -18,15 +18,25 @@ fetch('http://localhost:3000/drinks')
             cocktailName.innerText = drink.strDrink
             instructions.innerText= drink.strInstructions
             ingredients.innerText= " "
+            measures.innerText= " "
             const li = document.createElement('li')
             const li2 = document.createElement('li')
             const li3 = document.createElement('li')
             const li4 = document.createElement('li')
+            const list = document.createElement('li')
+            const list2 = document.createElement('li')
+            const list3 = document.createElement('li')
+            const list4 = document.createElement('li')
             li.innerText = drink.strIngredient1
             li2.innerText = drink.strIngredient2
             li3.innerText = drink.strIngredient3
             li4.innerText = drink.strIngredient4
+            list.innerText = drink.strMeasure1
+            list2.innerText = drink.strMeasure2
+            list3.innerText = drink.strMeasure3
+            list4.innerText = drink.strMeasure4
             ingredients.append(li, li2, li3, li4)
+            measures.append(list, list2, list3, list4)
         })
         cocktailImgs.append(img)
     })
