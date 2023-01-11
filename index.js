a301540d7cf02a4cf309a70419cfd4b599c2f176
avinash singh <avinash8.as@gmail.com>
2023-01-11 17:05:11 -0500

    render images onto page

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const cocktailImgs = document.querySelector("#cocktail-menu")
 
 
-
+fetch('http://localhost:3000/drinks')
+.then(resp => resp.json())
+.then(drinks => {
+    drinks.forEach(drink => {
+        const img = document.createElement('img')
+        img.src = drink.strDrinkThumb
+        cocktailImgs.append(img)
+    })
+})
